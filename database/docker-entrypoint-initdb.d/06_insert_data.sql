8d3a600d4d6a093c2360a2e4f582a20ce73c3867
furtiwile <furtiwile@gmail.com>
2025-11-29 13:49:10 +0100

    [DB] Update

--- database/docker-entrypoint-initdb.d/06_insert_data.sql
+++ database/docker-entrypoint-initdb.d/06_insert_data.sql
@@ -6,20 +6,20 @@ INSERT INTO users_db.user_roles (role_name) VALUES
   ('Audio & Music Stagist'),
   ('Animation Worker');
   
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('sysadmin', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 1, 'sa@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('sysadmin', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 1, 'sa@a.a', 0, NULL);
 
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('admin', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 2, 'a@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('admin', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 2, 'a@a.a', 0, NULL);
 
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('project_manager', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 3, 'pm@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('project_manager', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 3, 'pm@a.a', 0, NULL);
 
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('analytics_development_manager', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 4, 'adm@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('analytics_development_manager', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 4, 'adm@a.a', 0, NULL);
   
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('audio_music_stagist', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 5, 'ams@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('audio_music_stagist', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 5, 'ams@a.a', 0, NULL);
 
-  INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
-  VALUES ('animation_worker', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 6 , 'aw@a.a', 0, NULL);
+  -- INSERT INTO users_db.users (username, password_hash, user_role, email, is_deleted, weekly_working_hour_sum) 
+  -- VALUES ('animation_worker', '$2b$10$pWZUArmH0en/D8scIGgMfeEwDo88NPXv/4FE97GeA42TfZ3M0.GTu', 6 , 'aw@a.a', 0, NULL);
