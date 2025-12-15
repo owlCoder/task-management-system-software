b69886a5d0cc2e0c094ed91a28f917912eab2589
Mark-ic <markodragojevicc@gmail.com>
2025-12-15 14:41:54 +0100

    BugFix

--- infrastructure/microservices/mail-microservice/src/app.ts
+++ infrastructure/microservices/mail-microservice/src/app.ts
@@ -7,10 +7,11 @@ import cors from "cors";
 const app = express();
 
 const corsOrigin = process.env.CORS_ORIGIN ?? "*";
+const corsMethods = process.env.CORS_METHODS?.split(",").map(m => m.trim()) ?? ["POST"];
 
 app.use(cors({
   origin: corsOrigin,
-  methods: "POST",
+  methods: corsMethods,
 }));
 
 app.use(express.json());
