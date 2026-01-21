f45dbbfb94cbe1cde612299cc5839533c03e5673
Teodora Skaric <skaricteodora03@gmail.com>
2026-01-21 21:41:45 +0100

    DTO correction for review

--- infrastructure/microservices/version-control-microservice/src/Helpers/Converter/toReviewDTO.ts
+++ infrastructure/microservices/version-control-microservice/src/Helpers/Converter/toReviewDTO.ts
@@ -3,9 +3,13 @@ import { TaskReviewDTO } from "../../Domain/DTOs/TaskReviewDTO";
 
 export function toReviewDTO(review: Review): TaskReviewDTO {
   return {
+    reviewId: review.reviewId,
     taskId : review.taskId,
     authorId : review.authorId,
     status : review.status, 
     time : review.time,
+    reviewedBy: review.reviewedBy,      
+    reviewedAt: review.reviewedAt,      
+    commentId: review.commentId 
     };
 }
