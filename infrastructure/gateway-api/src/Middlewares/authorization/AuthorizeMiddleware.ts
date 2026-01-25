8adf45ec4c037c2410127c2e0c17c7a9283d7a75
RD4 <rdakic@protonmail.com>
2026-01-25 05:43:39 +0100

    [Gateway-API]: reverted authorization middleware

--- infrastructure/gateway-api/src/Middlewares/authorization/AuthorizeMiddleware.ts
+++ infrastructure/gateway-api/src/Middlewares/authorization/AuthorizeMiddleware.ts
@@ -17,43 +17,24 @@ import { logger } from "../../Infrastructure/logging/Logger";
  * @returns A middleware function that checks if the user's role is authorized. If authorized, it calls next(). Otherwise, it responds with a 403 status and an error message.
  */
 export const authorize = (...permittedRoles: UserRole[]) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    const user = req.user as AuthTokenClaimsType;
+  	return (req: Request, res: Response, next: NextFunction): void => {
+    	const user = req.user as AuthTokenClaimsType;
 
-    const normalize = (v: unknown) =>
-      String(v ?? "")
-        .trim()
-        .toLowerCase()
-        .replace(/&/g, "and")
-        .replace(/\s+/g, "_");
+    	if (!user || !permittedRoles.includes(user.role)) {
+			const message = "Access denied";
 
-    const rawRole = (user as any)?.role;
-    const rawPermitted = permittedRoles;
+			logger.warn({
+				service: "Gateway",
+				code: "AUTHORIZATION_ERR",
+				method: req.method,
+				url: req.url,
+				ip: req.ip
+			}, message);
 
-    const strictAllowed = !!user && rawPermitted.includes(rawRole as any);
+			res.status(403).json({ message: message });
+			return;
+    	}
 
-    const userNorm = normalize(rawRole);
-    const permittedNorm = rawPermitted.map((r) => normalize(r));
-    const normAllowed = !!user && permittedNorm.includes(userNorm);
-
-    console.log("=== AUTH DEBUG ===");
-    console.log("FILE:", __filename);
-    console.log("URL:", req.method, req.originalUrl);
-    console.log("raw role:", rawRole);
-    console.log("raw permitted:", rawPermitted);
-    console.log("strictAllowed:", strictAllowed);
-    console.log("userNorm:", userNorm);
-    console.log("permittedNorm:", permittedNorm);
-    console.log("normAllowed:", normAllowed);
-    console.log("==================");
-
-    // koristi normAllowed (ili strictAllowed) – bitno je da vidiš šta je true
-    if (!normAllowed) {
-      const message = "Access denied";
-      res.status(403).json({ message });
-      return;
-    }
-
-    next();
-  };
-};
+    	next();
+  	};
+}
