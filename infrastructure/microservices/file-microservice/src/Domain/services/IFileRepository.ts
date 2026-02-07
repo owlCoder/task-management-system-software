ed9a09aebd30bfd577545b060a383bead487253c
DarkMaster964 <nemanjavojnov@gmail.com>
2026-02-07 12:53:13 +0100

    [File] Removed unused file

--- infrastructure/microservices/file-microservice/src/Domain/services/IFileRepository.ts
+++
@@ -1,9 +0,0 @@
-import { UploadedFile } from '../models/UploadedFile';
-import { Result } from '../types/Result';
-
-export interface IFileRepository {
-  create(file: UploadedFile): Promise<Result<UploadedFile>>;
-  getById(fileId: number): Promise<Result<UploadedFile | null>>;
-  delete(fileId: number): Promise<Result<boolean>>;
-  getByAuthorId(authorId: number): Promise<Result<UploadedFile[]>>;
-}
