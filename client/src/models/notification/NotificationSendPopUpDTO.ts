79912a6eb8e346f2651a1bb918382b3dc338fd4f
Mateja Mikanović <mateja.mikanovic@gmail.com>
2026-02-07 13:35:28 +0100

    [Notify] removed unused dto's

--- client/src/models/notification/NotificationSendPopUpDTO.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationType } from "../../enums/NotificationType";
-
-export interface NotificationSendPopUpProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onSend: (title: string, content: string, type: NotificationType) => void;
-  loading?: boolean;
-  className?: string;
-}
